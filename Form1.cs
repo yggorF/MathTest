9b62b1af7fcf396b118459e9d209f3c67e54dd32
Ivan Ratushinskiy <iratushinakiy@appnow.com>
2023-06-26 05:16:15 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ratushinsky36
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        int addend1;
+        int addend2;
+
+        int minuend;
+        int subtrahend;
+ 
+        int multiplicand;
+        int multiplier;
+
+        int dividend;
+        int divisor;
+
+        int timeLeft;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            addend1 = random.Next(51);
+            addend2 = random.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            sum.Value = 0;
+
+            minuend = random.Next(1, 101);
+            subtrahend = random.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            multiplicand = random.Next(2, 11);
+            multiplier = random.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            divisor = random.Next(2, 11);
+            int temporaryQuotient = random.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            timeLeft = 30;
+            timeLabel.Text = "30 секунд";
+            timer1.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+            timeLabel.BackColor = Color.White;
+        }
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("Вы ответили на все вопросы правильно!",
+                                "МОЛОДЕЦ!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " секунд";
+                if (timeLeft <= 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Время вышло!";
+                MessageBox.Show("Вы не успели во время.", "СОРИ!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void sum_ValueChanged(object sender, EventArgs e)
+        {
+            if (addend1 + addend2 == sum.Value)
+            {
+                playSound_rightAnswer();
+            }
+        }
+
+        private void difference_ValueChanged(object sender, EventArgs e)
+        {
+            if (minuend - subtrahend == difference.Value)
+            {
+                playSound_rightAnswer();
+            }
+        }
+
+        private void product_ValueChanged(object sender, EventArgs e)
+        {
+            if (multiplicand * multiplier == product.Value)
+            {
+                playSound_rightAnswer();
+            }
+        }
+
+        private void quotient_ValueChanged(object sender, EventArgs e)
+        {
+            if (dividend / divisor == quotient.Value)
+            {
+                playSound_rightAnswer();
+            }
+        }
+        public void playSound_rightAnswer()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"sounds\sound.wav");
+            SoundPlayer player = new SoundPlayer(path);
+            player.Play();
+        }
+    }
+}
